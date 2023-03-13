ff4244e2d0ecdfdbf1bc4a59d80abfe72eff67b1
Bernardo Restrepo <bernardorestrepo@veryfi.com>
2023-03-12 19:53:40 -0500

    Update LogsViewController.swift

--- LensW9Demo/LogsViewController.swift
+++ LensW9Demo/LogsViewController.swift
@@ -28,7 +28,7 @@ class LogsViewController: UIViewController {
                                                           url: URL)
         let settings = VeryfiLensSettings(with: jsonSettings)
         // Specific document types for this demo (VeryfiLens-Receipts)
-        settings.documentTypes = ["receipt", "bill"]
+        settings.documentTypes = ["w9"]
         settings.showDocumentTypes = true
         
         VeryfiLens.shared().delegate = self
